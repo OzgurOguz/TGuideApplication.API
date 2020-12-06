3181bfc20ca36320f4b835c829aa7fce36b0c6f9
OzgurOguz <ozgur.oguz@hotmail.com>
2020-12-06 14:46:30 +0300

    Modifications 2

--- TGuideApplication.Servicee/MessageQueue/TGuidePublisher.cs
+++ TGuideApplication.Servicee/MessageQueue/TGuidePublisher.cs
@@ -1,9 +1,12 @@
-﻿using Newtonsoft.Json;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
 using RabbitMQ.Client;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TGuideApplication.Core.DbModels;
 using TGuideApplication.Core.IMessageQueue;
 using TGuideApplication.Core.IRepository;
 using TGuideApplication.Core.Models;
@@ -12,20 +15,23 @@ using TGuideApplication.Servicee.Services;
 namespace TGuideApplication.MessageQueue
 {
 
-
     public class TGuidePublisher : IPublisher
     {
 
-        public TGuidePublisherModelRepository _tGuidePublisherModelRepository;
-        public TGuidePublisher(TGuidePublisherModelRepository tGuidePublisherModelRepository)
+        public ITGuidePublisherModelRepository _tGuidePublisherModelRepository;
+        public IConfigurationRoot Configuration { get; set; }
+
+        public TGuidePublisher(ITGuidePublisherModelRepository tGuidePublisherModelRepository, IOptions<Settings> settings)
         {
             _tGuidePublisherModelRepository = tGuidePublisherModelRepository;
+            Configuration = settings.Value.IConfigurationRoot;
+
         }
 
         public void Publisher()
         {
             var factory = new ConnectionFactory();
-            factory.Uri = new Uri("amqps://nohtjnhs:ov2OEjCyIJuw09Oum-bmsed6Fx_DPGFm@jaguar.rmq.cloudamqp.com/nohtjnhs");
+            factory.Uri = new Uri(Configuration.GetSection("RabbitMqConnection:Uri").Value);  
             using (var connection = factory.CreateConnection())
             {
                 using (var channel = connection.CreateModel())
